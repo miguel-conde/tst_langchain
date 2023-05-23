6e5520b3593b69b69516f055d2987adc9ff87032
mcondedesimon <mcondedesimon@deloitte.es>
2023-05-23 15:20:07 +0200

    config.ini + medio  crash course P. Loeber

--- PYTHON/util_funs/globalsettings.py
+++ PYTHON/util_funs/globalsettings.py
@@ -1,4 +1,5 @@
 import os
+import configparser
 
 class prjSettings():
     def __init__(self):
@@ -18,6 +19,7 @@ class prjSettings():
 
 the_folders = prjSettings()
 
+the_folders.dir_root  = os.path.abspath(os.path.dirname(__file__)) # Project root is defined by globalsettings.py location
 the_folders.DIR_DATA = os.path.join(os.getcwd(), "data")
 the_folders.DIR_DATA_RAW = os.path.join(the_folders.DIR_DATA, "raw")
 the_folders.DIR_DATA_CLEAN = os.path.join(the_folders.DIR_DATA, "clean")
@@ -28,6 +30,23 @@ the_files = prjSettings()
 
 the_files.PROBE_PDF = os.path.join(the_folders.DIR_DATA, "77009321.pdf")
 
+the_files.cfg_file = os.path.join(the_files.dir_root, "config.ini")
+
+## CONSTANTS
+
 the_constants = prjSettings()
 
 the_constants.COLLECTION = "forecasting_ccb"
+
+
+## CONFIG FILE
+prj_cfg = prjSettings()
+
+config = configparser.ConfigParser()
+
+config.read(the_files.cfg_file)
+
+prj_cfg.serveraliveinterval = config['DEFAULT']['Serveraliveinterval']
+prj_cfg.compression         = config['DEFAULT']['Compression']
+prj_cfg.compressionlevel    = config['DEFAULT']['Compressionlevel']
+prj_cfg.forwardx11          = config['DEFAULT']['Forwardx11']
